6f4c26b5ed1c6cc47c5cbfd2a9807fbe5aeaea24
kulklex <kulklex@gmail.com>
2023-03-12 11:33:20 +0100

    SubHeading component impl

--- components/SubHeading.tsx
+++ components/SubHeading.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import images from '../constants/images'
+import Image from 'next/image'
 
-type Props = {}
+type Props = {
+  title: string,
+}
 
-export default function SubHeading({}: Props) {
+export default function SubHeading({title}: Props) {
   return (
-    <div>SubHeading</div>
+    <div className='mb-4 lg:mb-8'>
+      <p className='text-white font-serif italic font-extrabold capitalize text-lg'>
+        {title}
+      </p>
+      <Image src={images.spoon} alt="subheading-img" className='w-[45px]' />
+    </div>
   )
 }
