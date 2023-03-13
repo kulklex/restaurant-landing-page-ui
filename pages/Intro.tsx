0bc75c1eed33963a9ba0ca56d7377d61a2f5507d
kulklex <kulklex@gmail.com>
2023-03-13 01:10:12 +0100

    Intro Page Impl

--- pages/Intro.tsx
+++ pages/Intro.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+
 
 type Props = {}
 
 export default function Intro({}: Props) {
+  const videoRef = useRef<HTMLVideoElement>(null)
+  
+  const [isPlaying, setIsPlaying] = useState<boolean>(false) 
+  
+  const handleVideo = () => {
+    setIsPlaying((prev) => !prev)
+
+    if (isPlaying) {
+      videoRef.current?.pause()
+    } else {
+      videoRef.current?.play()
+    }
+  }
+
   return (
-    <div>Intro</div>
+    <div className='h-[100vh] relative'>
+      <video ref={videoRef} src="/meal.mp4" loop controls={false} muted className='w-full h-full object-cover'/>
+
+      <div className='absolute inset-0 bg-[rgba(0,0,0,0.30)] flex justify-center items-center'>
+        <div onClick={handleVideo} className='flex justify-center items-center w-full h-full rounded-[50%] border-solid border-[#DCCA87] cursor-pointer'>
+          {isPlaying 
+          ? (<BsPauseFill color="#fff" fontSize={30} />) 
+          : (<BsFillPlayFill color="#fff" fontSize={30} />)
+          }
+        </div>
+      </div>
+    </div>
   )
 }
